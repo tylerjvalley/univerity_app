fd3453f310ebc488dffdaa7ba8300821f3126eda
Tyler Valley <tvalley93@gmail.com>
2019-02-16 13:41:34 -0500

    Create students table and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create(short_name: "CS101", name: "Intro to Computer Science", description: "A thorough into to Computer Science and software development.")
+Course.create(short_name: "CS102", name: "Intro to Computer Science 2", description: "Continuation of CS101")
+Course.create(short_name: "CS112", name: "Object Oriented Programming", description: "A look into the concept of object oriented programming using Python.")
+Course.create(short_name: "CS201", name: "C++ Programming", description: "Intermediate CS class focusing on the language C++")
