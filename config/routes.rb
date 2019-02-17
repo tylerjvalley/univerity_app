36fd53f6112e301ea379a36abdcb146e0f110eb1
Tyler Valley <tvalley93@gmail.com>
2019-02-16 19:12:14 -0500

    Update students, add studemts show page and create new students

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
+  resources :students, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
