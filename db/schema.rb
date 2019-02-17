0289cd3a8a1ca9913d5a703c65b32655a9bd5258
Tyler Valley <tvalley93@gmail.com>
2019-02-16 20:04:45 -0500

    Add password column to students table and add password to forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_183042) do
+ActiveRecord::Schema.define(version: 2019_02_17_004442) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_02_16_183042) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
