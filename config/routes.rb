bb62c4b34c289302e05cf8b34c3e8691bca80f34
Tyler Valley <tvalley93@gmail.com>
2019-02-18 14:48:34 -0500

    Add course enrollment, add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'course_enroll', to: 'student_courses#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
